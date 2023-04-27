2e23476fd45b056d8e7158c9615280395f6a5fd0
cemtaskin <cemtaskin82@gmail.com>
2023-04-27 21:05:46 +0300

    Button Action Listener Added

--- awt2.java
+++ awt2.java
@@ -1,18 +1,30 @@
 import java.awt.Button;
+import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class awt2 {
+public class awt2 implements ActionListener{
+    Frame frame =new Frame();
+    Label label = new Label("Employee id:");
+    Label dialogLabel = new Label();
+
+    Button button = new Button("Submit");
+    TextField  textField=new TextField();
+    Dialog dialog;
+    
     public awt2(){
-        Frame frame =new Frame();
-        Label label = new Label("Employee id:");
-        Button button = new Button("Submit");
-        TextField  textField=new TextField();
-
+       
+        dialog=new Dialog(frame, "Attention", true);
+        dialogLabel.setBounds(10, 10, 100, 30);
+        dialog.add(dialogLabel);
         label.setBounds(20,80, 80, 30);
         textField.setBounds(20, 100, 80, 30);
         button.setBounds(100, 100, 80, 30); 
+
+        button.addActionListener(this);
 
         frame.add(label);
         frame.add(textField);
@@ -22,5 +34,16 @@ public class awt2 {
         frame.setTitle("Employee Info");
         frame.setLayout(null);
         frame.setVisible(true);
+
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        dialogLabel.setText(textField.getText());
+        dialog.setVisible(true);
+        
+    }
+
+    
+
 }
